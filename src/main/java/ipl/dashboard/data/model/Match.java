4082ee5c6307b25a271d1715bc09d3c0e0ef6fcd
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-08-05 17:04:11 +0300

    Ingesting data

--- src/main/java/ipl/dashboard/data/model/Match.java
+++ src/main/java/ipl/dashboard/data/model/Match.java
@@ -1,23 +1,28 @@
 package ipl.dashboard.data.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
+@Entity
 public class Match {
-    private long id;
-    private String city;
-    private LocalDate date;
-    private String playerOfMatch;
-    private String venue;
-    private String team1;
-    private String team2;
-    private String tossWinner;
-    private String tossDecision;
-    private String matchWinner;
-    private String result;
-    private String resultMargin;
 
-    private String umpire1;
-    private String umpire2;
+    @Id
+    private long      id;
+    private String    city;
+    private LocalDate date;
+    private String    playerOfMatch;
+    private String    venue;
+    private String    team1;
+    private String    team2;
+    private String    tossWinner;
+    private String    tossDecision;
+    private String    matchWinner;
+    private String    result;
+    private String    resultMargin;
+
+    private String    umpire1;
+    private String    umpire2;
 
     public long getId() {
         return id;
