ab050d655f374fea2f395914d1583fe36792d7c6
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-08-06 23:25:36 +0300

    Preparing additional data using JPQL IPL Dashboard

--- src/main/java/ipl/dashboard/data/MatchDataProcessor.java
+++ src/main/java/ipl/dashboard/data/MatchDataProcessor.java
@@ -48,6 +48,8 @@ public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
         match.setTossWinner(matchInput.getToss_winner());
         match.setTossDecision(matchInput.getToss_decision());
 
+        match.setMatchWinner(matchInput.getWinner());
+
         match.setResult(matchInput.getResult());
         match.setResultMargin(matchInput.getResult_margin());
 
---
+++ src/main/java/ipl/dashboard/data/model/Team.java
@@ -0,0 +1,67 @@
+package ipl.dashboard.data.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String teamName;
+    private long totalMatches;
+    private long totalWins;
+
+    public Team(String teamName, long totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
+
+    public Team() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public long getTotalMatches() {
+        return totalMatches;
+    }
+
+    public void setTotalMatches(long totalMatches) {
+        this.totalMatches = totalMatches;
+    }
+
+    public long getTotalWins() {
+        return totalWins;
+    }
+
+    public void setTotalWins(long totalWins) {
+        this.totalWins = totalWins;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Team{");
+        sb.append("teamName='").append(teamName).append('\'');
+        sb.append(", totalMatches=").append(totalMatches);
+        sb.append(", totalWins=").append(totalWins);
+        sb.append('}');
+        return sb.toString();
+    }
+}
