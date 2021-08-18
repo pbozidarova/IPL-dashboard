a7b98b4361ebe30a605be1b06465b3add80f1c3d
Petya Kuzmanova <p.bozidarova@gmail.com>
2021-08-18 10:57:10 +0300

    UI IPL Dashboard - second part

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -2,7 +2,9 @@ import { React, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
+import { YearSelector } from '../components/YearSelector';
 
+import "./MatchPage.scss";
 
 export const MatchPage = () => {
     
@@ -22,18 +24,22 @@ export const MatchPage = () => {
 
             fetchMatches();
 
-        }, []
+        }, [teamName, year]
     );
-
 
     return (
         <div className="MatchPage">
-            <h1>Match page</h1>            
-
-            {   matches
-                    .map(match => <MatchDetailCard teamName = {teamName} match = {match}/>)
-            }
-
+            <div className="year-selector">
+                <h3>Select year</h3>
+                <YearSelector teamName={teamName}/>
+            </div>
+            <div>
+                <h1 className="page-heading">{teamName} matches in {year}</h1>            
+                    
+                {   matches
+                        .map(match => <MatchDetailCard teamName = {teamName} match = {match}/>)
+                }
+            </div>
         </div>
   );
 }
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,5 +1,5 @@
 import { React, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { MatchDetailCard } from '../components/MatchDetailCard';
 import { MatchSmallCard } from '../components/MatchSmallCard';
 import { PieChart } from 'react-minimal-pie-chart';
@@ -57,10 +57,11 @@ export const TeamPage = () => {
                     .slice(1)
                     .map(match => <MatchSmallCard teamName = {team.teamName} match = {match}/>)
             }
+            
             <div className="more-link">
-                <a href="#">More ></a>
+                <Link to={`/teams/${teamName}/matches/${process.env.REACT_APP_DATA_END_YEAR}`}>More ></Link>
             </div>
-        
+
         </div>
   );
 }
